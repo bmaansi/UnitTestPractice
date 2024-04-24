22e346cc8eb9d6aa79ccb1e20c7943fb7faf2f41
bmaansi <bhardwajmaansi@gmail.com>
2024-04-24 11:53:30 -0700

    saving work

--- Password.cpp
+++ Password.cpp
@@ -10,6 +10,8 @@ using std::string;
   end of the string). The function is case-sensitive so 'Z' is different than
   'z' and any ASCII characters are allowed.
 */
+
+
 int Password::count_leading_characters(string phrase){
   int repetition = 1;
   int index = 0;
@@ -23,25 +25,15 @@ int Password::count_leading_characters(string phrase){
 
 bool Password::has_mixed_case(string password) {
   int has_both = 0;
-  // for(char c : password) {
-  //   if (c > 97 && c < 122 ) {
-  //     has_both++;
-  //   } else if (c > 65 && c < 90){
-  //     has_both++;
-  //   }
-
-  //   // if (islower(c)) {
-  //   //   has_both++;
-  //   // } else if (isupper(c)) {
-  //   //   has_both++;
-  //   // }
-  // }
-
-  for(int i(0); i < password.length(); i++) {
-    if (islower(password[i])) {
+  bool checkedLower = false;
+  bool checkedUpper = false;
+  for(char c : password) {
+    if (islower(c) && !checkedLower) {
       has_both++;
-    } else if (password[i]) {
+      checkedLower = true;
+    } else if (isupper(c) && !checkedUpper) {
       has_both++;
+      checkedUpper = true;
     }
   }
 
@@ -51,3 +43,46 @@ bool Password::has_mixed_case(string password) {
     return false;
   }
 }
+
+Password::Password() {
+  pass_history.push_back("ChicoCA-95929");
+}
+
+
+void Password::set(string p) {
+  int all_true = 0;
+  bool exists = false;
+  if(p.size() >= 8 && p.size() < 20) {
+    all_true++;
+  }
+  if (count_leading_characters(p) <= 3) {
+    all_true++;
+  } 
+  if (has_mixed_case(p)) {
+    all_true++;
+  } 
+  for (string password : pass_history) {
+    if (p == password) {
+      exists = true;
+      break;
+    }
+  }
+  if (all_true == 3 && !exists) {
+    pass_history.push_back(p);
+  }
+
+}
+
+
+/*
+  receives a string and authenticates it against the latest password in the
+  pass_history, returning true for an exact match or false when it does not match
+  or if a password has not been set.
+  */
+bool Password::authenticate(string p) {
+  if (pass_history[pass_history.size()-1] == p) {
+    return true;
+  } 
+  return false;
+}
+
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -47,34 +47,89 @@ TEST(PasswordTest, four_dollarsigh_letter_password)
 TEST(PasswordTest, one_upper)
 {
 	Password my_password;
-	bool actual = my_password.count_leading_characters("L");
+	bool actual = my_password.has_mixed_case("L");
 	ASSERT_EQ(false, actual);
 }
 
 TEST(PasswordTest, one_lower)
 {
 	Password my_password;
-	bool actual = my_password.count_leading_characters("m");
+	bool actual = my_password.has_mixed_case("m");
 	ASSERT_EQ(false, actual);
 }
 
 TEST(PasswordTest, one_upper_one_lower)
 {
 	Password my_password;
-	bool actual = my_password.count_leading_characters("Lm");
+	bool actual = my_password.has_mixed_case("Lm");
 	ASSERT_EQ(true, actual);
 }
 
 TEST(PasswordTest, one_lower_one_upper)
 {
 	Password my_password;
-	bool actual = my_password.count_leading_characters("mL");
+	bool actual = my_password.has_mixed_case("mL");
 	ASSERT_EQ(true, actual);
 }
 
 TEST(PasswordTest, two_lower)
 {
 	Password my_password;
-	bool actual = my_password.count_leading_characters("mm");
+	bool actual = my_password.has_mixed_case("mm");
 	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, random_symbols_and_ints)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("#$*(!$!*$!@)#01924");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, uppers_lowers_ints)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("AKFJSHsjkdg039258");
+	ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, lowers_ints)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("sjkdg039258");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, uppers_ints)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("AKFJSH039258");
+	ASSERT_EQ(false, actual);
+}
+
+// TEST(PasswordTest, constructor) {
+// 	Password my_password;
+// 	my_password.set("nj90NJ");
+// 	ASSERT_EQ("nj90NJ", my_password.pass_history[my_password.pass_history.size()-1]);
+// }
+
+TEST(PasswordTest, constructor) {
+	Password my_password;
+	//my_password.set("nj90NJ");
+	bool actual = my_password.authenticate("ChicoCA-95929");
+	ASSERT_EQ(true, actual);
+}
+
+TEST(PasswordTest, bad_password) {
+	Password my_password;
+	my_password.set("nj90NJ");
+	bool actual = my_password.authenticate("nj90NJ");
+	ASSERT_EQ(false, actual);
+}
+
+TEST(PasswordTest, good_password) {
+	Password my_password;
+	my_password.set("nj90NJnjn");
+	bool actual = my_password.authenticate("nj90NJnjn");
+	ASSERT_EQ(true, actual);
 }
